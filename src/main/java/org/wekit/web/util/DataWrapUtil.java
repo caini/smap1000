7248141133365155513f40b62bbb964c99dae864
huangweili <hwlchina@wekit.org>
2012-03-24 16:15:52 +0800

    提交测试

--- src/main/java/org/wekit/web/util/DataWrapUtil.java
+++ src/main/java/org/wekit/web/util/DataWrapUtil.java
@@ -4,13 +4,27 @@ import org.springframework.ui.Model;
 
 public class DataWrapUtil {
 
-	public static String ObjectToJson(String extend,Object object,Model model)
+	/**
+	 * 将对象转转换成json文件
+	 * @param object
+	 * @param model
+	 * @return
+	 */
+	public static String ObjectToJson(Object object,Model model)
 	{
 		
 		return null;
 	}
 	
-	
+	/**
+	 * 将对像转换成xml文件
+	 * @param object
+	 * @param model
+	 * @return
+	 */
+	public static String ObjectToXml(Object object,Model model){
+		return null;
+	}
 	
 	
 }
