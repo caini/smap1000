fe83ede6fd443ab4bbe3e280c90e45237355bde1
huangweili <hwlchina@wekit.org>
2012-04-04 21:55:52 +0800

    取号功能实现

--- src/main/java/org/wekit/web/service/impl/CodeServiceImpl.java
+++ src/main/java/org/wekit/web/service/impl/CodeServiceImpl.java
@@ -10,13 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.wekit.web.IPaginable;
-import org.wekit.web.db.Pagination;
 import org.wekit.web.db.dao.CodeDao;
+import org.wekit.web.db.dao.CodeRuleDao;
 import org.wekit.web.db.dao.CodeSequenceDao;
-import org.wekit.web.db.dao.ExtendCodeDao;
 import org.wekit.web.db.dao.TempCodeDao;
 import org.wekit.web.db.model.Code;
-import org.wekit.web.db.model.TempCode;
+import org.wekit.web.db.model.CodeRule;
 import org.wekit.web.service.CodeService;
 
 /**
@@ -31,16 +30,19 @@ public class CodeServiceImpl implements CodeService {
 	@Autowired
 	@Qualifier("codeDao")
 	private CodeDao			codeDao;
-	
-	
+
 	@Autowired
 	@Qualifier("tempCodeDao")
-	private TempCodeDao tempCodeDao;
-	
+	private TempCodeDao		tempCodeDao;
+
 	@Autowired
 	@Qualifier("codeSequenceDao")
-	private CodeSequenceDao codeSequenceDao;
-	
+	private CodeSequenceDao	codeSequenceDao;
+
+	@Autowired
+	@Qualifier("codeRuleDao")
+	private CodeRuleDao		codeRuleDao;
+
 	@Override
 	public Code getCode(Long id) {
 		if (id != null && id > 0) {
@@ -51,7 +53,7 @@ public class CodeServiceImpl implements CodeService {
 
 	@Override
 	public Code getCode(String code) {
-		if(StringUtils.isEmpty(code))
+		if (StringUtils.isEmpty(code))
 			return null;
 		return this.codeDao.getCode(code);
 	}
@@ -78,9 +80,9 @@ public class CodeServiceImpl implements CodeService {
 
 	@Override
 	public List<Code> getCodesWithPagination(IPaginable paginable) {
-		if(paginable!=null)
+		if (paginable != null)
 			return codeDao.getCodesWithPaginable(paginable);
-		else 
+		else
 			return codeDao.getAllCodes();
 	}
 
@@ -88,7 +90,7 @@ public class CodeServiceImpl implements CodeService {
 	public Code addCode(Code code) {
 		return this.codeDao.addCode(code);
 	}
-		
+
 	public CodeDao getCodeDao() {
 		return codeDao;
 	}
@@ -113,6 +115,14 @@ public class CodeServiceImpl implements CodeService {
 		this.codeSequenceDao = codeSequenceDao;
 	}
 
+	public CodeRuleDao getCodeRuleDao() {
+		return codeRuleDao;
+	}
+
+	public void setCodeRuleDao(CodeRuleDao codeRuleDao) {
+		this.codeRuleDao = codeRuleDao;
+	}
+
 	@Override
 	public boolean cancelCode(Long codeId) {
 		// TODO Auto-generated method stub
@@ -125,62 +135,87 @@ public class CodeServiceImpl implements CodeService {
 		return false;
 	}
 
+	/**
+	 * 独立取号和批量取号的差别是独立取号有限查找可用的序列号，在生成新的序列号，而批量操作只生成联系的新号而不关心旧的号码
+	 */
 	@Override
 	public Code fetchCode(String rule, String unitCode, String locationCode, String docCode, String creater, String createId, String note) {
-		// TODO Auto-generated method stub
+
+		return null;
+
+	}
+
+	public Code batchCode(String rule, String unitCode, String locationCode, String docCode, String creater, String createId, String note, int batchSize) {
+
+		// TODO
 		return null;
 	}
-	
-	public Code batchCode(String rule,String unitCode,String locationCode,String docCode,String creater,String createId,String note,int batchSize){
-		//TODO
-		return null;
-	}
-	
-	
-	
+
 	/**
 	 * 返回[n]中n的长度
+	 * 
 	 * @param mask
 	 * @return
 	 */
-	public MaskParser paserMask(String mask){
-		Map<String, Integer> param=new HashMap<String, Integer>();
-		Calendar calendar=Calendar.getInstance();
-		int year=calendar.get(Calendar.YEAR);
-		int month=calendar.get(Calendar.MONTH);
-		int day=calendar.get(Calendar.MONDAY);
-		
-		if(mask.indexOf("[yyyy]")>0){
-			mask=mask.replace("[yyyy]", String.valueOf(year));
+	public MaskParser paserMask(String mask) {
+		Map<String, Integer> param = new HashMap<String, Integer>();
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.MONDAY);
+
+		if (mask.indexOf("[yyyy]") > 0) {
+			mask = mask.replace("[yyyy]", String.valueOf(year));
 			param.put("year", year);
 		}
-		if(mask.indexOf("[yy]")>0){
-			mask=mask.replace("[yy]", String.valueOf(year%100));
+		if (mask.indexOf("[yy]") > 0) {
+			mask = mask.replace("[yy]", String.valueOf(year % 100));
 			param.put("year", year);
 		}
-		if(mask.indexOf("[mm]")>0){
-			if(month>9)
-				mask=mask.replace("[mm]", String.valueOf(month));
+		if (mask.indexOf("[mm]") > 0) {
+			if (month > 9)
+				mask = mask.replace("[mm]", String.valueOf(month));
 			else
-				mask=mask.replace("[mm]", "0"+String.valueOf(month));
+				mask = mask.replace("[mm]", "0" + String.valueOf(month));
 			param.put("month", month);
 		}
-		if(mask.indexOf("[dd]")>0){
-			if(day>9){
-				mask=mask.replace("[dd]", String.valueOf(day));
-			}else{
-				mask=mask.replace("[dd]","0"+String.valueOf(day));
+		if (mask.indexOf("[dd]") > 0) {
+			if (day > 9) {
+				mask = mask.replace("[dd]", String.valueOf(day));
+			} else {
+				mask = mask.replace("[dd]", "0" + String.valueOf(day));
 			}
 			param.put("day", day);
 		}
-		int left=mask.indexOf('[');
-		if(left<0)
-		{
-			return new MaskParser(param, mask,0);
+		int left = mask.indexOf('[');
+		if (left < 0) {
+			return new MaskParser(param, mask, 0);
 		}
-		int right=mask.indexOf(']');
-		
-		return new MaskParser(param, mask,right-left-1);
+		int right = mask.indexOf(']');
+
+		return new MaskParser(param, mask, right - left - 1);
 	}
-	
+
+	/**
+	 * 判断传递的参数是否有效
+	 * 
+	 * @param codeRule
+	 * @param unitCode
+	 * @param locationCode
+	 * @param docCode
+	 * @return
+	 */
+	public boolean checkRule(String codeRule, String unitCode, String locationCode, String docCode) {
+		String[] rules = codeRule.split("-");
+		if (rules.length != 4)
+			return false;
+		if (!(rules[0].endsWith("x") || rules.equals(unitCode)))
+			return false;
+		if (!(rules[1].equals("xxx") || rules[1].equals(locationCode)))
+			return false;
+		if (!(rules[2].equals("xxx") || rules[2].equals(docCode)))
+			return false;
+		return true;
+	}
+
 }
