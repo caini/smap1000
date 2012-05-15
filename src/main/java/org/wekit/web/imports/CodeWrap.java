b78a6cd4ebbda41f76f7d2ccd47836bd36b4f7e7
huangweili <hwlchina@wekit.org>
2012-05-16 00:01:32 +0800

    d

---
+++ src/main/java/org/wekit/web/imports/CodeWrap.java
@@ -0,0 +1,17 @@
+package org.wekit.web.imports;
+
+import java.io.Serializable;
+
+/**
+ * wrap参数封装类
+ * @author HuangWeili
+ *
+ */
+public class CodeWrap implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -5416344810147626221L;
+
+}
---
+++ src/main/java/org/wekit/web/imports/ExtendCodeWrap.java
@@ -0,0 +1,17 @@
+package org.wekit.web.imports;
+
+import java.io.Serializable;
+
+/**
+ * 无规则编码封装类
+ * @author HuangWeili
+ *
+ */
+public class ExtendCodeWrap  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -522536752898052236L;
+
+}
